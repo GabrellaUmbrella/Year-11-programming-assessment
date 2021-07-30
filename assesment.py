32f48a664a90843a01f27514086edee0cdec2a12
Gabrielle Smythe <GSmythe@stu.kerikerihigh.ac.nz>
2021-07-30 12:12:53 +1200

    I think i finshed the quiz

--- assesment.py
+++ assesment.py
@@ -1,40 +1,47 @@
 import requests
-import random	
+import random
+from html import unescape
 
+trys = 3
 answers_corrct = 0
 
 response = requests.get('https://opentdb.com/api.php?amount=20&category=31&type=multiple')
 questions = response.json()['results']
-print('\033[1;36;40m' + ''' 
-*~ ~ ~ ~ ~ ~ ~ Welcome to the Anime Quiz!!~ ~ ~ ~ ~ ~ ~*
+#This is how i got the questions and answers into my quiz by using an API!
+print('\033[1;34;40m' + ''' 
+*~ ~ ~ ~ ~ ~ ~ Welcome to the Anime Quiz!!~ ~ ~ ~ ~ ~ ~ ~*
 ''')
+
 for i, question in enumerate(questions):
-    print(f'---------Question {i + 1}/20------------')
-    print(question['question'])
-    all_answers = question['incorrect_answers']
-    all_answers.append(question['correct_answer'])
+    print('\033[1;36;40m' + f'---------Question {i + 1}/20------------')
+    print(unescape(question['question']))
+    all_answers = unescape(question['incorrect_answers'])
+    all_answers.append(unescape(question['correct_answer']))
     random.shuffle(all_answers)
     for answers in all_answers:
-        print('\033[1;35;40m' + answers)
-
+        print('\033[1;35;40m' + unescape(answers))
+#The block above is the code asking a question and then giving multiple answers to choose from!
+    trys = 3
     while True:
-        user_answer = input('\033[1;36;40m' + '\nWrite down which one you think is the correct answer!(spell it correctly please)\n')
-        if user_answer == '':
-            print('\033[1;31;40m' + "That's not a valid answer!(remember to spell it correctly)")
-            print('\033[1;36;40m')
-        elif user_answer in question['correct_answer']:
+        user_answer = input('\033[1;36;40m' + f'\nWrite down which one you think is the correct answer!(spell it correctly please, You only get {trys} try/s left!)\n')
+        if user_answer.lower() == question['correct_answer'].lower():
             answers_corrct += 1
             print('\033[1;32;40m' + '\nYou got it right ')
             print('\033[1;36;40m')
             break
-        elif user_answer in question['incorrect_answers']:
-            print('\033[1;33;40m' + "\nThat is incorrect, the corrct answer was")
-            print(question['correct_answer'])
+        elif user_answer.lower() in [question.lower() for question in question['incorrect_answers']]:
+            print('\033[1;30;40m' + "\nThat is incorrect, the corrct answer was")
+            print(unescape(question['correct_answer']))
             print('\033[1;36;40m')
             break
         else:
-            print('\033[1;31;40m' + "That's not a valid answer!(remember to spell it correctly)")
-            print('\033[1;36;40m')
-        
-print('\033[1;35;40m' + f'Thank you for playing my quiz, you got {answers_corrct}/20 questions correct!')
+            trys = trys - 1
+            print('\033[1;31;40m' + f"That's not a valid answer!(remember to spell it correctly, You only get {trys} try/s left!)" + '\033[1;36;40m')
+            if trys == 0:
+                print('\033[1;30;40m' + "\nThat is incorrect, the corrct answer was")
+                print(unescape(question['correct_answer']) + '\033[1;36;40m')
+                break
+#This block above is using the users input and giving them feedback and an error message!
+
+print('\033[1;34;40m' + f'Thank you for playing my quiz, you got {answers_corrct}/20 questions correct!')
 
--- quiztester.py
+++ quiztester.py
@@ -3,6 +3,9 @@ import requests
 response = requests.get('https://opentdb.com/api.php?amount=10&category=15&type=multiple')
 #response = requests.get('https://opentdb.com/api.php?amount=20&category=32&type=multiple')
 #response = requests.get('https://opentdb.com/api.php?amount=20&category=31&type=multiple')
+#https://opentdb.com/api.php?amount=20&category=15&difficulty=easy&type=multiple
+
+#https://opentdb.com/api.php?amount=20&category=32&difficulty=easy&type=multiple
 questions = response.json()['results']
 
 for i, question in enumerate(questions):
@@ -11,6 +14,21 @@ for i, question in enumerate(questions):
     print(question['correct_answer'])
     for wrong_ans in question['incorrect_answers']:
         print(wrong_ans)
+
+# import requests
+# import random
+
+# response = requests.get('https://opentdb.com/api.php?amount=20&category=15&type=multiple')
+# questions = response.json()['results']
+
+# for i, question in enumerate(questions):
+#     print(f'-------------Questions {i + 1}--------------')
+#     print(question['question'])
+#     all_answers = question['incorrect_answers']
+#     all_answers.append(question['correct_answer'])
+#     random.shuffle(all_answers)
+#     for answers in all_answers:
+#         print(answers)
 
 
 
